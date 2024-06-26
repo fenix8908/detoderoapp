7623062d653e150fccda675e84edcfca386c2ee9
JORGE <marialuci1019@gmail.com>
2024-06-25 23:04:40 -0500

    Se agrega validacion al interceptor para evitar que tome el login, se agrega manejo de respuesta del refresh token

--- src/app/interceptores/auth.interceptor.ts
+++ src/app/interceptores/auth.interceptor.ts
@@ -1,16 +1,36 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const loginUrl = 'http://localhost:8080/authenticate';
   const token = localStorage.getItem('jwtToken');
   const refreshToken = localStorage.getItem('jwtTokenRefresh');
+
+  // Verifica si la solicitud es para el endpoint de login
+  // Si es el login, simplemente pasa la solicitud sin modificarla
+  if (req.url.includes(loginUrl)) {
+    return next(req);
+  }
 
   if (token && refreshToken) {
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
         RefreshToken: `${refreshToken}`,
-      }
+      },
     });
   }
-  return next(req);
+  return next(req).pipe(
+    tap((event) => {
+      if (event instanceof HttpResponse) {
+        const newAccesToken = event.headers.get('newAccessToken');
+        const newRefreshToken = event.headers.get('newRefreshToken');
+
+        if(newAccesToken && newRefreshToken){
+           localStorage.setItem('jwtToken',`${newAccesToken}`)
+           localStorage.setItem('jwtTokenRefresh',`${newRefreshToken}`)
+        }
+      }
+    })
+  );
 };
