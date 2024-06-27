5c77576c798f04d4d4a1619ba2af3fbe9e841cb5
JORGE <marialuci1019@gmail.com>
2024-06-26 22:42:25 -0500

    Se crea tabla para listar clientes

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -1,31 +1,44 @@
+import { Cliente} from './../models/cliente';
 import { Component, OnInit } from '@angular/core';
 import { ClienteService } from '../service/cliente.service';
-import { Cliente } from '../models/cliente';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cliente',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './cliente.component.html',
   styleUrl: './cliente.component.css',
 })
 export class ClienteComponent implements OnInit {
-  clienteResponse!: Cliente;
+  clienteResponse: Cliente[] = [];
   constructor(private clienteService: ClienteService) {}
-
 
   ngOnInit(): void {
     this.consultarClientes();
   }
 
-  consultarClientes() {
+  consultarClientes(){
     this.clienteService.obtenerClientes().subscribe({
       next: (respuesta) => {
-        this.clienteResponse = respuesta;
-        console.log(this.clienteResponse)
+        respuesta.forEach((e,i) => {
+          const cliente = new Cliente(
+            e.id,
+            e.nombre,
+            e.apellido,
+            e.email,
+            e.telefono,
+            e.direccion,
+            e.fechaRegistro
+          );
+          this.clienteResponse[i] = cliente;
+        });
+        return this.clienteResponse;
       },
-      error: (err) => {console.log(err)},
-      complete: () => {console.log('Proceso terminado con exito')},
+      error: (err) => {
+        console.log(err);
+        return [];
+      }
     });
   }
 }
--- src/app/service/cliente.service.ts
+++ src/app/service/cliente.service.ts
@@ -10,10 +10,10 @@ export class ClienteService {
   urlServicio: string = 'http://localhost:8080/clientes/listado';
   constructor(private http: HttpClient) {}
 
-  obtenerClientes(): Observable<Cliente> {
+  obtenerClientes(): Observable<Cliente[]> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json'
     });
-    return this.http.get<Cliente>(this.urlServicio, { headers });
+    return this.http.get<Cliente[]>(this.urlServicio, { headers });
   }
 }
