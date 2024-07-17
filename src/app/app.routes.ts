ff74883e846371f4e462d6fd27b0f469804ecc23
JORGE <marialuci1019@gmail.com>
2024-07-17 18:44:01 -0500

    Creacion del componente home e intalacion de angular Material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ClienteComponent } from './cliente/cliente_lista.component';
 import { permisosGuard } from './guards/permisos.guard';
+import { HomeComponent } from './home/home/home.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
   {
     path: 'clientes',
     component: ClienteComponent,
